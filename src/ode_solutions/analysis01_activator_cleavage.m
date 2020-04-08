9751638cb0af701c5834e50a091ff5dd646792b3
nlammers371 <nlammers@berkeley.edu>
2020-04-08 12:48:01 -0700

    Well...drat

--- src/ode_solutions/analysis01_activator_cleavage.m
+++ src/ode_solutions/analysis01_activator_cleavage.m
@@ -1,3 +1,5 @@
+% script to determine if activator cleavage by Cas13 can replicate key
+% features in NCR experimental data
 clear
 close all
 addpath('../utilities')
@@ -7,24 +9,27 @@ mkdir(DataPath)
 
 % specify project to load
 project = 'ncr_general';
+sub_project = 'activator_cleavage';
 % load 
 load([DataPath project '_setup.mat'])
 
 % make figure path
-FigPath = ['../../fig/ode_studies/' project '/' ];
+FigPath = ['../../fig/ode_studies/' project '/' sub_project '/' ];
 mkdir(FigPath)
 
-t_max = 2e3;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% Specify model %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Filter for key pathway interactions only. Include Activator cleavage
-
 % filters for catalytic and specific interactions 
 cat_spec_filter = catalytic_activity_flag_vec | specific_activity_flag_vec; 
 % filter to remove catalytic reactions that involve guide cleavage
 cat_g_filter = ~contains(reaction_string_cell,{'G1Z','G2Z'});
 % filter for all reactions for which guid RNA is an input or product
 cg_indices = find(strcmp(full_atom_cell,'G1')|strcmp(full_atom_cell,'G2')|...
-    strcmp(full_atom_cell,'C1')|strcmp(full_atom_cell,'C2'));
+    strcmp(full_atom_cell,'C1')|strcmp(full_atom_cell,'C2')|...
+    strcmp(full_atom_cell,'T1Z')|strcmp(full_atom_cell,'T1'));
 free_g_filter = max(stoichiometry_matrix(cg_indices,:)~=0)==0;
 
 final_filter = free_g_filter & cat_g_filter & cat_spec_filter;
@@ -55,121 +60,111 @@ for i = 1:numel(reaction_rate_index)
 end
 reaction_unit_index = unique(reaction_units);
 
-%% initial reactant concentrations
-S0 = 200;
-C1_G10 = 50;
-C2_G20 = .8;
-T10 = 0;
-T20 = 0;
-T2I0 = 20;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%% set key parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+t_max = 3600; % duration of time to solve for
+
+%%%% specify initial reactant concentrations 
 % vectors for substitutions
-init_sym_array = {'T1','T2','S' 'T2I' 'G1:C1' 'G2:C2'};
-init_val_vec = [T10 T20 S0 T2I0 C1_G10 C2_G20];
+init_sym_array = {'T2','S' 'T2I' 'G1:C1' 'G2:C2'};
+init_val_vec_ncr =    [0    200 20 50 0.8]; % for simulated negative NCR raction
+init_val_vec_target = [3e-2 200  0 0  0.8]; % for target "sub critical" reaction
+
 % initialize concentration vector. Everything not explicitly assigned taken
 % to be zero
-y0_vec = zeros(size(reactant_cell_truncated));
+y0_vec_ncr = zeros(size(reactant_cell_truncated));
+y0_vec_target = zeros(size(reactant_cell_truncated));
 for i = 1:numel(init_sym_array)
     species = init_sym_array{i};
     spec_indices = find(strcmp(reactant_cell_truncated,species));
-    y0_vec(spec_indices) = init_val_vec(i);
+    y0_vec_ncr(spec_indices) = init_val_vec_ncr(i);
+    y0_vec_target(spec_indices) = init_val_vec_target(i);
 end
+y0_cell = {y0_vec_ncr};
+%%%%%%%%%%%%%%%%%
+%%% Specify which rate parameters to fit and specify values for "given"
+%%% parameters
 
+% specify subset of parameters to fit
+fit_indices = [1:8];
 % define basic on/off and cleavaeg rates
 kon = 0.25;
 koff_s = 1;
-kcat_cas = 200*2e-5; % basal cas cleavage rate
-kcat_ratio = 1/2e-5;
+kcat_cas = 200*2e-7; % basal cas cleavage rate
+kcat_ratio = 1/2e-7;
 koff_ns = 1.5e3; 
-% define substrate cleavage susceptibilities
+% define substrate cleavage susceptibilities (these are the key parameters
+% here)
 sS = 1; % reporter
-sT1 = 1; % activator 1
-sT2 = 1; % activator 2
-sT2I = 1; % caged activator
+sT2 = 1e-4; % activator 2
+sT2I = 1e-1; % caged activator
+
 
 % rate vector to substitute into
-rate_vec_val = rate_vec_zero_order_truncated;
-
+rate_vec_target = rate_vec_zero_order_truncated;
 % generate valued long rate vec
 for r = 1:numel(reaction_unit_index)
-    rr = reaction_unit_index(r);    
-    
-    rate_vec_val = subs(rate_vec_val,rr,eval(rr));
-    
+    rr = reaction_unit_index(r);        
+    rate_vec_target = subs(rate_vec_target,rr,eval(rr));    
 end
-rate_vec_val = double(rate_vec_val);
-%% solve ODEs numerically
+rate_vec_target = double(rate_vec_target);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%% solve ODEs for target numerically %%%%%%%%%%%%%%%%%%
+% 
 close all
 f_indices = find(contains(reactant_cell_truncated,'SZ'));
-ti_indices = find(contains(reactant_cell_truncated,'T2I'));
-t2_indices = find(contains(reactant_cell_truncated,'T2')...
-    &~contains(reactant_cell_truncated,'T2I')...
-    &~contains(reactant_cell_truncated,'T2Z'));
-t2z_indices = find(contains(reactant_cell_truncated,'T2Z'));
+[t_target,y_target] = ode15s(@(t,y) ncr_solver(t,y,rate_vec_target,stoich_mat_truncated),[0 t_max],y0_vec_target);
+f_target = sum(y_target(:,f_indices),2);
+[t_ncr_null,y_ncr_null] = ode15s(@(t,y) ncr_solver(t,y,rate_vec_target,stoich_mat_truncated),[0 t_max],y0_vec_ncr);
+f_ncr_null = sum(y_ncr_null(:,f_indices),2);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%% attempt to fit rate parameters %%%%%%%%%%%%%%%%%%
 
-[t_pos,y_pos] = ode15s(@(t,y) ncr_solver(t,y,rate_vec_val,stoich_mat_truncated),[0 t_max],y0_vec);
-
-
-figure;
-plot(t_pos,sum(y_pos(:,ti_indices),2))
-title('caged activator 2')
-
-figure;
-plot(t_pos,sum(y_pos(:,t2_indices),2))
-title('free activator 2')
-
-figure;
-plot(t_pos,sum(y_pos(:,t2z_indices),2))
-title('cleaved activator 2')
-
-figure;
-plot(t_pos,sum(y_pos(:,f_indices),2))
-title('reporter')
-
-%% attempt to fit rate parameters
-
-% specify subset of parameters to fit
-fit_indices = [1 2 3 4 7:9];
+% substitute "given" values into rate vector
 fixed_indices = find(~ismember(1:numel(reaction_unit_index),fit_indices));
 rate_vec_fit = rate_vec_zero_order_truncated;
 for r = fixed_indices
     rr = reaction_unit_index(r);        
     rate_vec_fit = subs(rate_vec_fit,rr,eval(rr));    
 end
-
+% convert this to a matlabFunction
 rate_vec_fun = matlabFunction(rate_vec_fit);
 
-% generate fake experimental data
-time_exp = t_pos;
-f_vec = sum(y_pos(:,f_indices),2);
-fluo_exp = f_vec;%interp1(t_pos,f_vec,time_exp);%sum(y_pos(:,f_indices),2);%interp1([0 t_max],[0,S0*.4],time_exp);
+% now set bounds for variables to be inferred
+ub_vec = NaN(size(fit_indices));
+lb_vec = NaN(size(fit_indices));
 rate_params_init = rand(size(fit_indices));
+iter = 1;
+for r = fit_indices
+    rate_init = eval(reaction_unit_index(r));        
+    ub_vec(iter) = 1e4*rate_init;
+    lb_vec(iter) = 1e-4*rate_init;
+    rate_params_init(iter) = eval(reaction_unit_index(r))*rand()*1e1+lb_vec(iter);
+    iter = iter + 1;
+end
 
-fit_fun = @(rate_params,time_exp) ode_objfunction(t_max,rate_params,rate_vec_fun,...
-    stoich_mat_truncated,y0_vec,f_indices,time_exp);
-%%
-options = optimoptions('lsqcurvefit','MaxIterations',50);
-kon = 0.25;
-koff_s = 1;
-kcat_cas = 200*2e-5; % basal cas cleavage rate
-kcat_ratio = 1/2e-5;
-koff_ns = 1.5e3; 
-% define substrate cleavage susceptibilities
-sS = 1; % reporter
-sT1 = 1; % activator 1
-sT2 = 1; % activator 2
-sT2I = 1; % caged activator
+% define fitting function
+fit_fun = @(rate_params,time_exp_array) ode_objfunction(t_max,rate_params,rate_vec_fun,...
+    stoich_mat_truncated,y0_cell,f_indices,time_exp_array);
 
-% set bounds for inference parameters
-ub = [1    1e7 10  5e3 10 10 10];
-lb = [2e-4 1e3 0.1 100 0 0 0];
+% set optimization options
+options = optimoptions('lsqcurvefit','MaxIterations',200);
 
+% generate interpolated vectors for fitting
+time_exp = 0:0.1:t_max;
+time_exp_array = [time_exp'];
+fluo_exp_array(:,1) = interp1(t_target,f_target,time_exp);
+% fluo_exp_array(:,2) = fluo_exp_array(:,1);
+% fluo_exp_array(:,2) = interp1(t_ncr_null,f_ncr_null,time_exp);
 
+% perform optimization
 tic
-rate_vec_out=lsqcurvefit(fit_fun,[.1 1e5 1 1e3 1 1 1],time_exp,fluo_exp,lb,ub,options);
+rate_vec_out=lsqcurvefit(fit_fun,rate_params_init,time_exp_array,fluo_exp_array,lb_vec,ub_vec,options);
 toc
-%%
-initial_sol = fit_fun(rate_params_init,time_exp);
-final_sol = fit_fun(rate_vec_out,time_exp);
+
+initial_sol = fit_fun(rate_params_init,time_exp_array);
+final_sol = fit_fun(rate_vec_out,time_exp_array);
 % %%
 % fluo_factor = 12000;
 % close all
